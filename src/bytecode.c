740319561c6fa75f2741757ce9c36b67e07c0356
Aaron Pribadi <aaron.pribadi@gmail.com>
2021-09-21 19:29:20 -0400

    z

--- src/bytecode.c
+++ src/bytecode.c
@@ -36,21 +36,36 @@ static void bc_show(bc_t t) {
   }
 }
 
-// bc_chunk
+// bc_prog
+
+typedef struct {
+  i64 count;
+  bc_t code[];
+} bc_prog_t;
+
+static void bc_prog_show(bc_prog_t * t) {
+  i64 count = t->count;
+
+  for (i64 i = 0; i < count; i ++) {
+    bc_show(t->code[i]);
+  }
+}
+ 
+// bc_buf
 
 typedef struct {
   i64 count;
   i64 capacity;
   bc_t * code;
-} bc_chunk_t;
+} bc_buf_t;
 
-static void bc_chunk_init(bc_chunk_t * t) {
+static void bc_buf_init(bc_buf_t * t) {
   t->count = 0;
   t->capacity = 0;
   t->code = NULL;
 }
 
-static void bc_chunk_add(bc_chunk_t * t, bc_t bc) {
+static void bc_buf_add(bc_buf_t * t, bc_t bc) {
   if (t->count + 1 > t->capacity) {
     i64 old_capacity = t->capacity;
     i64 new_capacity = old_capacity < 8 ? 8 : 2 * old_capacity;
@@ -69,11 +84,16 @@ static void bc_chunk_add(bc_chunk_t * t, bc_t bc) {
   t->code[t->count ++] = bc;
 }
 
-static void bc_chunk_show(bc_chunk_t * t) {
+static bc_prog_t * bc_buf_prog(bc_buf_t * t) {
   i64 count = t->count;
-  bc_t * code = t->code;
+
+  bc_prog_t * prog = allocate(sizeof(bc_prog_t) + sizeof(bc_t) * count);
+
+  prog->count = count;
 
   for (i64 i = 0; i < count; i ++) {
-    bc_show(code[i]);
+    prog->code[i] = t->code[i];
   }
+
+  return prog;
 }
